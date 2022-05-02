0f45e7199a6bfa19d1c266a022d1d08db41d8b2c
Elizabeth Phung <99864765+Elizabeth-phung@users.noreply.github.com>
2022-05-02 12:05:53 -0700

    Update IWorkShopService.cs

--- DotNet/Service/Interface/IWorkShopService.cs
+++ DotNet/Service/Interface/IWorkShopService.cs
@@ -7,13 +7,6 @@ namespace Sabio.Services.Interfaces
 {
     public interface IWorkShopService
     {
-        int Add(WorkShopAddRequest model, int userId);
-        void Delete(int id);
-        WorkShop Get(int id);
-        Paged<WorkShop> GetAll(int pageIndex, int pageSize);
-        Paged<WorkShop> Search(int pageIndex, int pageSize, string query);
-        List<WorkShop> GetFive();
-        void Update(WorkShopUpdateRequest model, int userId, int id);
         List<WorkshopVenues> GetByGeo(int radius, double latitude, double longitude);
     }
 }
