cf9af1d36db073ad6306f3f47b4042e58b9f01d7
Elizabeth-phung <eliphu1997@gmail.com>
2022-05-02 12:05:30 -0700

    interface workshop

---
+++ DotNet/Service/Interface/IWorkShopService.cs
@@ -0,0 +1,19 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests.Workshop;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IWorkShopService
+    {
+        int Add(WorkShopAddRequest model, int userId);
+        void Delete(int id);
+        WorkShop Get(int id);
+        Paged<WorkShop> GetAll(int pageIndex, int pageSize);
+        Paged<WorkShop> Search(int pageIndex, int pageSize, string query);
+        List<WorkShop> GetFive();
+        void Update(WorkShopUpdateRequest model, int userId, int id);
+        List<WorkshopVenues> GetByGeo(int radius, double latitude, double longitude);
+    }
+}
