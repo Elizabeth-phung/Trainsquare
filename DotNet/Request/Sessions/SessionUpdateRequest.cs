18ac6a4d77973cdf46da893773e5fe509ed099aa
Elizabeth Phung <99864765+Elizabeth-phung@users.noreply.github.com>
2022-05-13 15:41:48 -0700

    Update SessionUpdateRequest.cs

--- DotNet/Request/Sessions/SessionUpdateRequest.cs
+++ DotNet/Request/Sessions/SessionUpdateRequest.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Sessions
 {
     public class SessionUpdateRequest : SessionAddRequest, IModelIdentifier
     {
