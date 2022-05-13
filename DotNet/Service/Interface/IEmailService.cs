6dde302325252d9dcae3d40425ca1fe6f7659d67
Elizabeth Phung <99864765+Elizabeth-phung@users.noreply.github.com>
2022-05-13 15:42:05 -0700

    Update IEmailService.cs

--- DotNet/Service/Interface/IEmailService.cs
+++ DotNet/Service/Interface/IEmailService.cs
@@ -1,15 +1,3 @@
-﻿using Microsoft.AspNetCore.Http;
-using Sabio.Models.Requests;
-using Sabio.Models.Requests.ContactUs;
-using Sabio.Models.Requests.Email;
-using Sabio.Models.Requests.NewsletterSubscriptions;
-using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Services
 {
     public interface IEmailService
     {
