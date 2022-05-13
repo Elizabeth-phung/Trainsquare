ffa53b637805667e6f24dab27b1c20e1328b3d88
Elizabeth Phung <99864765+Elizabeth-phung@users.noreply.github.com>
2022-05-13 15:40:51 -0700

    Update Session.cs

--- DotNet/Model/Session.cs
+++ DotNet/Model/Session.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Domain
 {
     public class Session 
     {
